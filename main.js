a51c1aa771ea2ddad6251566fed65d6b615792c6
Tod Gentille <tod.gentille@gmail.com>
2019-03-14 12:51:42 -0700

    move main.js (and test) to project root

---
+++ main.js
@@ -0,0 +1,4 @@
+const logger = require('./util/logger')
+logger.info('hello node world!')
+// Leaving this outside the /src folder so that creating an npm package is more straight forward
+// and seems to be canonical way this is done by the packages I've looked at.
