2ef3a550eb8b0a2a50a1f6b76e27240e0064a1d8
Tod Gentille <tod.gentille@gmail.com>
2019-03-14 13:50:10 -0700

    include main.test.js explicitly

---
+++ src/util/logger.test.js
@@ -0,0 +1,11 @@
+const chai = require('chai')
+chai.should()
+
+
+describe('name or description of test', () => {
+  // beforeEach ( () =>{})
+  // afterEach ( () =>{})
+  it('${true should be true}', () => {
+    true.should.be.true
+  })
+})
